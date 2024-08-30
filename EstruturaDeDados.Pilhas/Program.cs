9f7134a5efd723efb8654a59bc832236b319524f
fabiokusaba <fabinhokb44@gmail.com>
2024-08-30 20:48:58 -0300

    Pilhas - Aula 11

---
+++ EstruturaDeDados.Pilhas/Program.cs
@@ -0,0 +1,7 @@
+﻿// Stack é o que a gente chamaria de pilha, pilha de coisas que a gente empilha, vamos dizer que a gente está lavando pratos e
+// a gente colocou um prato em cima do outro, uma pilha de pratos, e o que acontece numa pilha de pratos? Se a gente fez a nossa
+// pilha aqui qual é o mais fácil para pegar? A gente vai pegar o último prato que a gente empilhou, então a pilha é uma caso no
+// qual o último a ser colocado é o primeiro a ser retirado, esse comportamento a gente chama de LIFO (last in first out), um
+// exemplo que podemos pensar aqui é o CTRL+Z, o que ele faz? Ele vai desfazendo as últimas coisas que eu fiz, o histórico de 
+// navegação funciona da mesma forma, quando você vai voltar você volta do último para o primeiro, então sempre que você tiver um
+// problema em que você precisa ir na sequência e voltar na ordem do último para o primeiro a pilha vai ser a sua estrutura de dados
