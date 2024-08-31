875b97ca45323f5c399be323b79e51210add2cfc
fabiokusaba <fabinhokb44@gmail.com>
2024-08-31 16:52:04 -0300

    Dicionários - Aula 16

--- EstruturaDeDados.Dicionario/Program.cs
+++ EstruturaDeDados.Dicionario/Program.cs
@@ -22,3 +22,69 @@
 // pode ter gente depois, e aí toda vez que você procurar pela Suzana esse nome vai passar por uma função de hash que vai achar o mesmo
 // valor e aí ele sabe que a Suzana que estamos procurando é o 12465 e vai trazer a informação, por isso que é muito rápido, é como se
 // ele criasse um índice dentro dessa tabela de hash com todos que estão lá
+
+Dictionary<string, int> meuDicionario = new Dictionary<string, int>();
+
+meuDicionario.Add("chave1", 1);
+meuDicionario.Add("chave2", 2);
+meuDicionario.Add("chave3", 3);
+meuDicionario.Add("chave4", 1);
+
+int count = meuDicionario.Count;
+
+int value;
+// Acessando uma chave inexistente dessa forma direta vai dar erro, se existisse daria certo mas não é o tipo de coisa que a gente
+// vai fazer
+// value = meuDicionario["chave11"];
+
+// Nós utilizamos o TryGetValue para evitar uma exceção, esse método simplesmente vai te dar um true ou false, então se existir a
+// chave ele vai dar um true e ainda aqui no out ele vai colocar o valor que ele achou
+if (meuDicionario.TryGetValue("chave1", out value))
+{
+    Console.WriteLine("Valor encontrado: " + value);
+}
+else
+{
+    Console.WriteLine("Chave não encontrada.");
+}
+
+// Aqui estamos tentando adicionar um valor e novamente ele vai dar um true ou false, se ele conseguir adicionar ele dá um true, se
+// ele não conseguir ele dá um false
+if (meuDicionario.TryAdd("chave1", 1))  
+{
+    Console.WriteLine("Valor adicionado com sucesso.");
+}
+else
+{
+    Console.WriteLine("A chave já existe no dicionário.");
+}
+
+// O ContainsKey vai pegar essa "chave1" e vai olhar lá naquela tabela de hash se tem ou não tem, aqui ele simplesmente verifica se tem
+// ele não vai trazer o valor ele só vai olhar
+if (meuDicionario.ContainsKey("chave1"))
+{
+    Console.WriteLine("Chave encontrada.");
+}
+else
+{
+    Console.WriteLine("Chave não encontrada.");
+}
+
+// O ContainsValue é um uso com moderação de preferência não use, tudo o que vimos até agora era O(1), ou seja, se eu preciso adicionar
+// ele é O(1) porque é muito rápido é meio que constante ele vai olhar lá na tabela dele se já tem aquela chave e inclui, TryGet também
+// ele vai olhar tudo naquela tabela de hash então tudo é muito rápido, mas o ContainsValue não porque o ContainsValue ele não está
+// naquela tabela de hash porque o que tem lá é uma tabela de hash com as chaves e não com os valores, o que o dicionário precisa fazer
+// quando você fala verifica aí se contém o valor que eu quero? Ele vai item por item abrindo a chave até encontrar, então esse sim é
+// considerado O(n) porque a medida que você vai crescendo ele vai crescendo
+if (meuDicionario.ContainsValue(1))
+{
+    Console.WriteLine("Valor encontrado.");
+}
+else
+{
+    Console.WriteLine("Valor não encontrado.");
+}
+
+meuDicionario.Remove("chave1");
+
+meuDicionario.Clear();
