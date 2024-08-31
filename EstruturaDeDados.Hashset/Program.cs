dacda1a4ee934c816c0e466a1adacdfb756649d8
fabiokusaba <fabinhokb44@gmail.com>
2024-08-31 16:58:50 -0300

    Hashset - Aula 17

---
+++ EstruturaDeDados.Hashset/Program.cs
@@ -0,0 +1,5 @@
+﻿// Hashset você pode pensar como conjuntos, o conjunto que vem da matemática, ele também é generic aceitando diversos tipos
+// pensa no conjunto como sendo uma coleção que só pode ter um item de cada tipo, ou seja, não dá pra repetir, são únicos, o
+// conjunto vai utilizar aqui, com o próprio nome diz, uma função de hash pra transformar cada item específico em um código de
+// hash que vai ajudar não somente ele a saber que é único mas também ele localizar de forma muito rápida, então a grosso modo
+// tudo o que a gente fizer com hash a complexidade vai ser O(1) porque através do hash ele consegue achar rapidamente
