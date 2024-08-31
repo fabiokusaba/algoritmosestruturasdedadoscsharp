1b41598bbb2875992a92807b6c17acb569b63d4e
fabiokusaba <fabinhokb44@gmail.com>
2024-08-30 21:10:43 -0300

    Árvores - Aula 13

---
+++ EstruturaDeDados.Arvores/Program.cs
@@ -0,0 +1,9 @@
+﻿// Árvores composta por nós, o nó que dá origem a todos os demais nós chamamos de nó raiz ou root, os ramos seria qualquer nó
+// que tem outros nós ligados a ele, já as folhas são os nós que não tem outro nó ligado a ele, ligado no sentido de estar abaixo
+// dele
+// A gente sempre pode pensar em cada pedaço da nossa árvore como uma sub-árvore porque em algum momento a gente pode trabalhar com
+// recursão e aí as nomenclaturas seriam reordenadas
+// Nós iremos focar na árvore binária e nesse modelo de árvore a gente tem no máximo dois itens ligados a cada nó
+// O nó a gente pode pensar como algo dessa forma aqui, lembrando que estou me referindo aqui de uma árvore binária, ou seja, ele tem
+// no máximo dois outros nós ligados a ele, aqui eu colocaria o valor que eu quero, aqui eu teria um left e right que seria esquerda e
+// direita que simplesmente é pra dizer o que eu vou colocar no lado esquerdo e o que eu vou colocar no lado direito
